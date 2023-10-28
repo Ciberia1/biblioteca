1da8fd73d773b901b7f3509597b59be4341a589e
jesus.fdez1 <jesus.fdez1@alu.uclm.es>
2023-10-28 23:45:13 +0200

    Incorporación interfaz gestión obras
    Se añade un prototipo de interfaz para gestionar las obras y se modifica el gestor de interfaces

--- src/main/java/com/dominio/controladores/GestorInterfaces.java
+++ src/main/java/com/dominio/controladores/GestorInterfaces.java
@@ -1,5 +1,7 @@
 package com.dominio.controladores;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,36 +11,53 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dominio.entidades.*;
+import com.persistencia.ObraDAO;
 import com.persistencia.UsuarioDAO;
 
 @Controller
 public class GestorInterfaces {
 
-	    @Autowired
-	    private UsuarioDAO usuarioDAO;
+	@Autowired
+	private UsuarioDAO usuarioDAO;
+	private ObraDAO obraDAO;
 
-	    @GetMapping("/login")
-	    public String formularioLogin(Model modelo) {
-	        modelo.addAttribute("usuario", new Usuario());
-	        return "login";
-	    }
-	    
-	    @PostMapping("/login")
-	    public String enviarFormularioLogin(@ModelAttribute Usuario usuario, RedirectAttributes redirectAttributes) {
-
-	        Usuario usuarioExistente = usuarioDAO.findByDni(usuario.getDni());
-	        if (usuarioExistente == null || !usuarioExistente.getContrasena().equals(usuario.getContrasena())) {
-	            redirectAttributes.addAttribute("error", "invalid");
-	            return "redirect:/login";
-	        }
-
-	        // Código para manejar la información de inicio de sesión enviada...
-	        return "redirect:/inicio";  // Redirige a inicio.html si los datos de inicio de sesión son correctos
-	    }
-	    
-	    @GetMapping("/inicio")
-	    public String menuInicio(Model modelo) {
-	        modelo.addAttribute("usuario", new Usuario());
-	        return "inicio";
-	    }
+	@GetMapping("/login")
+	public String formularioLogin(Model modelo) {
+		modelo.addAttribute("usuario", new Usuario());
+		return "login";
 	}
+
+	@PostMapping("/login")
+	public String enviarFormularioLogin(@ModelAttribute Usuario usuario, RedirectAttributes redirectAttributes) {
+
+		Usuario usuarioExistente = usuarioDAO.findByDni(usuario.getDni());
+		if (usuarioExistente == null || !usuarioExistente.getContrasena().equals(usuario.getContrasena())) {
+			redirectAttributes.addAttribute("error", "invalid");
+			return "redirect:/login";
+		}
+
+		// Código para manejar la información de inicio de sesión enviada...
+		return "redirect:/inicio"; // Redirige a inicio.html si los datos de inicio de sesión son correctos
+	}
+
+	@GetMapping("/inicio")
+	public String menuInicio(Model modelo) {
+		modelo.addAttribute("usuario", new Usuario());
+		return "inicio";
+	}
+
+	@Autowired
+	public void ObraController(ObraDAO obraDAO) {
+		this.obraDAO = obraDAO;
+	}
+
+	@GetMapping("/gestion")
+	public String getObras(Model model) {
+	    List<Obra> obras = obraDAO.findAll();
+	    for (Obra obra : obras) {
+			model.addAttribute("obras", obras);
+		}
+	    return "gestion"; // Nombre del archivo HTML "gestion.html"
+	}
+
+}
